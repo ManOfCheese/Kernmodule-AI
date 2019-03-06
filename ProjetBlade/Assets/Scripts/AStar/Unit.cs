30f8b79a3243322cb041b3224f3fb5177bc5ecca
ManOfCheese <samwalet16@gmail.com>
2019-03-06 14:13:32 +0100

    Implemented A*
    Implemented A* pahtfinding for goblin

---
+++ ProjetBlade/Assets/Scripts/AStar/Unit.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Unit : MonoBehaviour {
+
+    public Transform target;
+    public float speed = 20;
+    private Vector3[] path;
+    private int targetIndex;
+
+    void Start() {
+        StartCoroutine(CheckPath());
+    }
+
+    IEnumerator CheckPath() {
+        while (true) {
+            PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+    public void OnPathFound(Vector3[] newPath, bool pathSuccessful) {
+        if (pathSuccessful) {
+            path = newPath;
+            targetIndex = 0;
+            StopCoroutine("FollowPath");
+            StartCoroutine("FollowPath");
+        }
+    }
+
+    IEnumerator FollowPath() {
+        if (path != null) {
+            Vector3 currentWaypoint = path[0];
+
+            while (true) {
+                if (transform.position == currentWaypoint) {
+                    targetIndex++;
+                    if (targetIndex >= path.Length) {
+                        yield break;
+                    }
+                    currentWaypoint = path[targetIndex];
+                }
+
+                transform.position = Vector3.MoveTowards(transform.position, currentWaypoint, speed * Time.deltaTime);
+                yield return null;
+
+            }
+        }
+        else {
+            yield break;
+        }
+    }
+
+    public void OnDrawGizmos() {
+        if (path != null) {
+            for (int i = targetIndex; i < path.Length; i++) {
+                Gizmos.color = Color.black;
+                Gizmos.DrawCube(path[i], Vector3.one);
+
+                if (i == targetIndex) {
+                    Gizmos.DrawLine(transform.position, path[i]);
+                }
+                else {
+                    Gizmos.DrawLine(path[i - 1], path[i]);
+                }
+            }
+        }
+    }
+}
---
+++ ProjetBlade/Assets/Scripts/Finite State Machine/StateMachine.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace StateMachine {
+    public class StateMachine<T> {
+
+        public State<T> CurrentState { get; private set; }
+        public T Owner;
+
+        //Constructor.
+        public StateMachine(T _owner) {
+            Owner = _owner;
+            CurrentState = null;
+        }
+
+        //Change our current state.
+        public void ChangeState(State<T> _newState) {
+            //If we are in a state.
+            if (CurrentState != null) {         
+                //Run the ExitState of our current state.
+                CurrentState.ExitState(Owner);  
+            }
+            //Set the current state to be our new state.
+            CurrentState = _newState;           
+            //Run the EnterState of our new state.
+            CurrentState.EnterState(Owner);     
+        }
+
+
+        //Update this state.
+        public void Update() {
+            //If we are in a state.
+            if (CurrentState != null) {
+                //Update that state.
+                CurrentState.UpdateState(Owner);  
+            }
+        }
+    }
+
+
+    //Abstract class containing all the functionality a state should have.
+    public abstract class State<T> {
+        public abstract void EnterState(T _owner);
+        public abstract void ExitState(T _owner);
+        public abstract void UpdateState(T _owner);
+        public abstract void UpdateTarget(T _owner);
+    }
+}
+
