c531e6a226b70125fcdd8693b87e08e1c24ab366
ManOfCheese <samwalet16@gmail.com>
2019-03-06 14:19:45 +0100

    Setup for state machine

---
+++ ProjetBlade/Assets/Scripts/Finite State Machine/Goblin/AgressiveState.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgressiveState : State {
+    public override void EnterState() {
+
+    }
+
+    public override void ExitState() {
+
+    }
+
+    public override void UpdateState() {
+
+    }
+}
--- ProjetBlade/Assets/Scripts/Finite State Machine/StateMachine.cs
+++ ProjetBlade/Assets/Scripts/Finite State Machine/StateMachine.cs
@@ -2,49 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace StateMachine {
-    public class StateMachine<T> {
+public class StateMachine {
 
-        public State<T> CurrentState { get; private set; }
-        public T Owner;
+    public State CurrentState { get; private set; }
 
-        //Constructor.
-        public StateMachine(T _owner) {
-            Owner = _owner;
-            CurrentState = null;
+    //Constructor.
+    public StateMachine() {
+        CurrentState = null;
+    }
+
+    //Change our current state.
+    public void ChangeState(State _newState) {
+        //If we are in a state.
+        if (CurrentState != null) {         
+            //Run the ExitState of our current state.
+            CurrentState.ExitState();  
         }
-
-        //Change our current state.
-        public void ChangeState(State<T> _newState) {
-            //If we are in a state.
-            if (CurrentState != null) {         
-                //Run the ExitState of our current state.
-                CurrentState.ExitState(Owner);  
-            }
-            //Set the current state to be our new state.
-            CurrentState = _newState;           
-            //Run the EnterState of our new state.
-            CurrentState.EnterState(Owner);     
-        }
-
-
-        //Update this state.
-        public void Update() {
-            //If we are in a state.
-            if (CurrentState != null) {
-                //Update that state.
-                CurrentState.UpdateState(Owner);  
-            }
-        }
+        //Set the current state to be our new state.
+        CurrentState = _newState;           
+        //Run the EnterState of our new state.
+        CurrentState.EnterState();     
     }
 
 
-    //Abstract class containing all the functionality a state should have.
-    public abstract class State<T> {
-        public abstract void EnterState(T _owner);
-        public abstract void ExitState(T _owner);
-        public abstract void UpdateState(T _owner);
-        public abstract void UpdateTarget(T _owner);
+    //Update this state.
+    public void Update() {
+        //If we are in a state.
+        if (CurrentState != null) {
+            //Update that state.
+            CurrentState.UpdateState();  
+        }
     }
+}
+
+//Abstract class containing all the functionality a state should have.
+public abstract class State {
+    public abstract void EnterState();
+    public abstract void ExitState();
+    public abstract void UpdateState();
 }
 
